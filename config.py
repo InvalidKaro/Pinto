9d6bd797731c6e8f6775e447b3294eb87bdd977d
Karo <76846752+InvalidKaro@users.noreply.github.com>
2023-10-15 12:04:52 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# config.py
+DISCORD_CLIENT_ID = 'YOUR_DISCORD_CLIENT_ID'
+DISCORD_CLIENT_SECRET = 'YOUR_DISCORD_CLIENT_SECRET'
+DISCORD_REDIRECT_URI = 'http://localhost:5000/discord_login'  # Set the redirect URI to match your application's settings
