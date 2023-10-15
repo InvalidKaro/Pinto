5e14f2735a44e7902bf7e37934d5bcf6132991a3
Karo <76846752+InvalidKaro@users.noreply.github.com>
2023-10-15 12:05:17 +0200

    Create run_dashboard.py

---
+++ run_dashboard.py
@@ -0,0 +1,5 @@
+# run_dashboard.py
+from dashboard import app  # Import your Flask app from the dashboard package
+
+if __name__ == '__main__':
+    app.run(debug=True)  # Run the Flask app with debug mode enabled
